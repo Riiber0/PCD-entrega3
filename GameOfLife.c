b0585906b072c260ef84e14d877b7e36360abe1b
Felipe Rosa <feliperibeirorosa@hotmail.com>
2022-11-28 16:22:29 -0300

    criacao das grids localmente

---
+++ GameOfLife.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <strings.h>
+#include "mpi.h"
+
+int** tab_init(int noProcesses){
+
+	int **ret = (int**)malloc(sizeof(int*) * 2048/noProcesses + 2);
+
+	for(int i = 0; i < 2048/noProcesses + 2; i++){
+		ret[i] = (int*)malloc(sizeof(int) * 2048);
+		bzero(ret[i], sizeof(int) * 2048);
+	}
+
+
+	return ret;
+
+}
+
+int main(int argc, char** argv){
+
+	int processId; /* rank dos processos */
+	int noProcesses; /* Número de processos */
+	int nameSize; /* Tamanho do nome */
+	char computerName[MPI_MAX_PROCESSOR_NAME];
+
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &noProcesses);
+	MPI_Comm_rank(MPI_COMM_WORLD, &processId);
+	MPI_Get_processor_name(computerName, &nameSize);
+
+	int **grid = tab_init(noProcesses);
+
+	if(!processId){
+		//GLIDER
+		int lin = 2, col = 0;
+		grid[lin  ][col+1] = 1;
+		grid[lin+1][col+2] = 1;
+		grid[lin+2][col  ] = 1;
+		grid[lin+2][col+1] = 1;
+		grid[lin+2][col+2] = 1;
+		 
+		//R-pentomino
+		lin =11; col = 30;
+		grid[lin  ][col+1] = 1;
+		grid[lin  ][col+2] = 1;
+		grid[lin+1][col  ] = 1;
+		grid[lin+1][col+1] = 1;
+		grid[lin+2][col+1] = 1;
+	}
+
+	MPI_Barrier(MPI_COMM_WORLD);
+	
+	return 0;
+
+}
